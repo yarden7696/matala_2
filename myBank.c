a7b7b5dc00a4a30d6813df4bc6583b7d3a69a74b
yarden7696 <yarden7696@gmail.com>
2019-11-20 06:38:04 -0500

    v1

--- myBank.c
+++ myBank.c
@@ -1,4 +1,11 @@
 #include<stdio.h>
+#include<stdbool.h>
+#define arr_size 50
+#define closed 0//mean i can use the account for new costumer
+#define opened 1//mean that i cant use the accounte 'cause alredy used!
+
+
+
  int account[50][2];
 
  int acount_amount = 0;
@@ -17,18 +24,20 @@ void openAccount()
 {
     int i;
     int a;
-	
+    int current_index = 0;
+	bool isavilble =false;
     for (i = 0;i< 50; i++) {
-        if((account[i][0] == 0))
+        if((account[i][0] == closed))
             {
+                isavilble = true;
                 break;
             }
-         //printf("%d , %d \n",i, account[i][0] );        
+          
     }
-     if(i<50){
+     if(isavilble){
         printf("initial deposit? ");
         scanf("%d", &a);
-        account[i][0] = 1;
+        account[i][0] = opened;
         account[i][1] = a;
         printf("number account: %d opened with income : %d\n" , i+901, account[i][1]);
         }
