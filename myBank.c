c94c53c3c2c4a651761d1480a93dd9ccfb87ab92
elonezra <elonezra1995@hotmail.com>
2019-11-20 15:53:21 +0200

    build deposit(), build closeAccount(), build showOpenAccount, build main menu

--- myBank.c
+++ myBank.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<unistd.h>
 #include<stdbool.h>
 #define arr_size 50
 #define closed 0//mean i can use the account for new costumer
@@ -18,7 +19,8 @@ int acount_amount = 0;
 
 void openAccount();
 void Deposit();
-
+void closeAccount();
+void showOpenAccounts();
 
 /////////////////////////////////////////
 ///////////     Methods     ////////////
@@ -26,10 +28,48 @@ void Deposit();
 
 int main()
 {
-while(1){
+    char operation = 'a';
+    printf("myBank Program\n");
+    printf("===============\n\n");
 
-	openAccount();	
-    Deposit();
+    
+    while(operation != 'e')
+    {
+        printf("Transaction type: ");
+        scanf("%c", &operation);
+        printf("\n");
+        switch (operation)
+        {
+        case 'o':
+            openAccount();
+            break;
+        case 'b':
+            /* code */
+            break;  
+        case 'd':
+            Deposit();
+            break; 
+        case 'w':
+            /* code */
+            break; 
+        case 'c':
+            closeAccount();
+            break; 
+        case 'i':
+            /* code */
+            break; 
+        case 'p':
+            showOpenAccounts();
+            break; 
+        case 'e':
+            /* code */
+            break;       
+
+        default:
+            //printf("wrong input!\n");
+            break;
+        }
+   while((getchar())!='\n');
 	}
 
 	return 1; 
@@ -72,7 +112,7 @@ void Deposit( )
     printf("Account number: ");
     scanf("%d",&account_num);
     account_num = account_num-901;
-    if((account_num < 50 || account_num >=0 )&& accounts[account_num][status] == 1)
+    if((account_num < 50 || account_num >=0 )&& accounts[account_num][status] == opened)
     {
         printf("Amount: ");
         scanf("%d",&deposit);
@@ -83,6 +123,49 @@ void Deposit( )
     {
         printf("Error: wrong account number or not opend!\n");
     }
+}
 
+/*
+the function get account number and close it
+*/
+void closeAccount()
+{
+    int account_num;
+    int deposit;
+    printf("Account number: ");
+    scanf("%d",&account_num);
+    account_num = account_num-901;
+    if((account_num < 50 || account_num >=0 )&& accounts[account_num][status] == opened)
+    {
+        accounts[account_num][status] = closed;
+        printf("account number %d is closed\n\n", account_num+901);
+       // printf("%d => %d", account_num+901, accounts[account_num][status]);
+    }
+    else
+    {
+        printf("Error: wrong account number or not opend!\n\n");
+    }
+}
+
+/*
+the function display all the open accounts and their balance
+*/
+void showOpenAccounts()
+{
+
+printf("Account number  |   balance\n");
+printf("----------------|----------\n");
+
+    for (int i = 0; i < arr_size; i++)
+    {
+    
+        if(accounts[i][status]==opened)
+        {
+            printf("%d\t\t|\t %d \n",i+901,accounts[i][balance]);
+            printf("----------------|----------\n");
+        }
+
+    }
+    printf("\n");
     
 }
