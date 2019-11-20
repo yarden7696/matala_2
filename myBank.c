a085a681340aa3e21c0d6e3dd27fc1afb5aff50c
elonezra <elonezra1995@hotmail.com>
2019-11-20 14:45:54 +0200

    format code, fixed openaccount(), build Deposit()

--- myBank.c
+++ myBank.c
@@ -3,31 +3,50 @@
 #define arr_size 50
 #define closed 0//mean i can use the account for new costumer
 #define opened 1//mean that i cant use the accounte 'cause alredy used!
+#define balance 1
+#define status 0
+
+
+int accounts[arr_size][2];
+int acount_amount = 0;
 
 
 
- int account[50][2];
+/////////////////////////////////////////
+///////////  Declerations   ////////////
+///////////////////////////////////////
 
- int acount_amount = 0;
 void openAccount();
+void Deposit();
+
+
+/////////////////////////////////////////
+///////////     Methods     ////////////
+///////////////////////////////////////
+
 int main()
 {
 while(1){
 
 	openAccount();	
+    Deposit();
 	}
 
 	return 1; 
         }
 
+
+/*
+the function get first deposit and register to new account
+*/
 void openAccount()
 {
-    int i;
-    int a;
-    int current_index = 0;
+    int index;
+    int deposit_amount;
+    
 	bool isavilble =false;
-    for (i = 0;i< 50; i++) {
-        if((account[i][0] == closed))
+    for (index = 0;index< 50; index++) {
+        if((accounts[index][0] == closed))
             {
                 isavilble = true;
                 break;
@@ -36,10 +55,34 @@ void openAccount()
     }
      if(isavilble){
         printf("initial deposit? ");
-        scanf("%d", &a);
-        account[i][0] = opened;
-        account[i][1] = a;
-        printf("number account: %d opened with income : %d\n" , i+901, account[i][1]);
+        scanf("%d", &deposit_amount);
+        accounts[index][0] = opened;
+        accounts[index][1] = deposit_amount;
+        printf("\nnumber account: %d opened with income : %d\n" , index+901, accounts[index][1]);
         }
 }
 
+/*
+the function get account number amount of money and print the new balance
+*/
+void Deposit( )
+{
+    int account_num;
+    int deposit;
+    printf("Account number: ");
+    scanf("%d",&account_num);
+    account_num = account_num-901;
+    if((account_num < 50 || account_num >=0 )&& accounts[account_num][status] == 1)
+    {
+        printf("Amount: ");
+        scanf("%d",&deposit);
+        accounts[account_num][balance] += deposit;
+        printf("now in account %d the balance is %d\n",account_num+901,accounts[account_num][balance]);
+    }
+    else
+    {
+        printf("Error: wrong account number or not opend!\n");
+    }
+
+    
+}
